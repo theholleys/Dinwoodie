ce7cf8cf4187641a5cc55b73ca022a1915466a50
WebH <max@webh.dev>
2022-07-03 17:00:31 +0100

    navigation animations

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const headerLinks = document.getElementsByClassName("header")[0]
+const barOne = document.getElementsByClassName("bar-one")[0]
+const barTwo = document.getElementsByClassName("bar-two")[0]
+const barThree = document.getElementsByClassName("bar-three")[0]
 
 toggleButton.addEventListener("click", (menuItems) => {
-    headerLinks.classList.toggle("active")
+    headerLinks.classList.toggle("active");
+    toggleButton.classList.toggle("active");
+    barOne.classList.toggle("active");
+    barTwo.classList.toggle("active");
+    barThree.classList.toggle("active");
 })
